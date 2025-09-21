7e10ed081ee8298c407a7b6c2010a3a5c44f5566
Ananta-git <anantalawatiforcsit@gmail.com>
2025-09-21 12:36:15 +0545

    components added Header.tsx

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <h2>My Website</h2>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
