4e67ab834a46b81fc613c6b2ab20b71924fe7f9f
Ananta-git <anantalawatiforcsit@gmail.com>
2025-09-23 15:29:03 +0545

    Newssection.tsx 3 items show

--- src/app/components/NewsSection.tsx
+++ src/app/components/NewsSection.tsx
@@ -1,24 +1,36 @@
 import Link from 'next/link';
-import { NewsItem } from '@/types';
-import { FeaturesSectionProps } from '@/types'; 
+import { DisplayOptions, NewsItem } from '@/types'; 
 
-interface NewsSectionProps{
+interface NewsSectionProps extends DisplayOptions {
     news: NewsItem[]
 }   
 
-export default function NewsSection({news}: NewsSectionProps){
+export default function NewsSection({
+    news,
+    showAll = false,
+    maxDisplay = 3
+    }: NewsSectionProps){
+    //Show only limited news if not showing all
+    const displayedNews = showAll ? news : news.slice(0, maxDisplay)
+    const hasMoreNews = news.length > maxDisplay && !showAll
     return(
         <section className="py-16">
             <div className="container mx-auto px-4">
                 <div className="flex justify-between items-center mb-12">
                     <h2 className="text-3xl font-bold text-gray-300"> Latest News & Announcements</h2>
-                    <Link href="/news" className="text-blue-600 hover:text-blue-800 font-semibold">
-                        View All →
-                    </Link>
-                </div>
+                    {/*Show View All button oly when there are more features*/}
+                    {hasMoreNews&&(
+                                <Link 
+                                    href="/features"
+                                    className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition duration-200"
+                                    >
+                                    View All Features
+                                </Link>
+                            )}
+                    </div>
 
                 <div className="grid md:grid-cols-3 gap-6">
-                    {news.map((item) => (
+                    {displayedNews.map((features, index) => (
                         <article 
                             key={item.id}
                             className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-200"
